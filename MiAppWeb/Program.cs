866b3e9b60b1ae7ee940e333e9ad56bbfb3146a1
Roberto Merino <merino@live.com>
2025-10-06 12:27:40 -0400

    App is running in port 5001

--- MiAppWeb/Program.cs
+++ MiAppWeb/Program.cs
@@ -1,10 +1,8 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
 
 
 var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.UseUrls("http://*:5000"); // Escuchar en el puerto 5000
+//builder.WebHost.UseUrls("http://*:5000"); // Escuchar en el puerto 5000
 
 var app = builder.Build();
 app.MapGet("/", () => "Hola Mundo");
